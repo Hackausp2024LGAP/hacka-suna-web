7c0ae0a83131d7870d92c03136c297d4557a79c3
mousedesvio <redbrickcar2@gmail.com>
2024-11-10 08:38:49 -0300

    resolvido

--- src/app/auth/sign_in/page.js
+++ src/app/auth/sign_in/page.js
@@ -1,21 +1,7 @@
-//import { PrismaClient } from '@prisma/client'
-//const prisma = new PrismaClient()
-
-import { Button } from "@/components/ui/button";
+import { SignIn } from "./_components/sign_in";
 
 export default function Page() {
     return (
-        <div className="rounded-xl w-80 h-96 bg-gray-100 flex align-middle justify-center">
-            <form id="form">
-                <p className="text-[2rem] text-center mb-12 mt-12">Log In</p>
-                <label className="text-lg">E-mail:</label><br></br>
-                <input type="text" id="email" name="email" className="p-1 mb-6 outline outline-1 rounded-sm"></input><br></br>
-                <label className="text-lg">Senha:</label><br></br>
-                <input type="password" id="pass" name="pass" className="p-1 outline outline-1 rounded-sm"></input><br></br>
-                <div className="justify-center flex">
-                    <Button type="submit" form="form" value="Submit" className="mt-6">Entrar</Button>
-                </div>
-            </form>
-        </div>
+        <SignIn/>
     )
 }
--- src/app/auth/sign_up/_components/sign_up.js
+++ src/app/auth/sign_up/_components/sign_up.js
@@ -1,63 +1,42 @@
 'use client'
-import React from "react";
+
+import { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "@/components/ui/tabs"
+import { FallbackMode } from "next/dist/lib/fallback";
+import { getUsers } from "@/script"
+import { redirect } from "next/navigation";
 
 export function Signup() {
-  const tabRef = React.useRef(null);
-  const emailRef = React.useRef(null);
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const users = prisma.user.findMany()
+  const emailRef = useRef(null);
 
+  const [canSign, setCanSign] = useState(false)
+
+  async function handleSubmit (event) {
+    event.preventDefault();
+    const users = await getUsers();
+    if(users.some((item) => item.email === emailRef.current.value)){
+        window.location.href = '/auth/sign_in'
+    }
+    else{
+      setCanSign(true)
+    }
   }
 
-  return (
-    <Tabs defaultValue="client" className="w-[400px]" ref={tabRef}>
-        <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="client">Cliente</TabsTrigger>
-            <TabsTrigger value="tecnico">Técnico</TabsTrigger>
-        </TabsList>
-        <TabsContent value="client">
-            <div className="rounded-xl w-80 h-96 bg-gray-100 flex align-middle justify-center">
-                <form id="form" onSubmit={handleSubmit}>
-                    <p className="text-[2rem] text-center mb-12 mt-12">Cadastre-se</p>
-                    <label className="text-lg">E-mail:</label><br></br>
-                    <input type="text" ref={emailRef} name="email" className="p-1 mb-6 outline outline-1 rounded-sm"></input><br></br>
-                    <div className="justify-center flex">
-                        <Button type="submit" form="form" value="Submit" className="mt-6" onClick={handleSubmit}>Cadastrar</Button>
-                    </div>
-                </form>
-            </div>
-        </TabsContent>
-        <TabsContent value="tecnico">
-            <div className="rounded-xl w-80 h-96 bg-gray-100 flex align-middle justify-center">
-                <form id="form" onSubmit={handleSubmit}>
-                    <p className="text-[2rem] text-center mb-12 mt-12">Cadastre-se</p>
-                    <label className="text-lg">E-mail:</label><br></br>
-                    <input type="text" ref={emailRef} name="email" className="p-1 mb-6 outline outline-1 rounded-sm"></input><br></br>
-                    <div className="justify-center flex">
-                        <Button type="submit" form="form" value="Submit" className="mt-6" onClick={handleSubmit}>Cadastrar</Button>
-                    </div>
-                </form>
-            </div>
-        </TabsContent>
-  </Tabs> 
+  return  (
+    <div>
+      {canSign ?
+        <></>
+      : 
+        <div className="rounded-xl w-80 h-96 bg-gray-100 flex align-middle justify-center">
+          <form id="form" onSubmit={handleSubmit}>
+              <p className="text-[2rem] text-center mb-12 mt-12">Cadastre-se</p>
+              <label className="text-lg">E-mail:</label><br></br>
+              <input type="text" ref={emailRef} name="email" className="p-1 mb-6 outline outline-1 rounded-sm"></input><br></br>
+              <div className="justify-center flex">
+                  <Button type="submit" form="form" value="Submit" className="mt-6" onClick={handleSubmit}>Cadastrar</Button>
+              </div>
+          </form>
+        </div>}
+    </div>
   )
 }
